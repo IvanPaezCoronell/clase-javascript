1188467c35e5dad64f4bdce4819cb5753c5ed68e
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-05-01 10:03:34 -0500

    Comentarios, Variables, Constantes

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+alert('¡Soy JavaScript!');
+
+// comentarios
+
+// comentario de una linea
+/* comentario
+de mas de una linea */
+
+
+
+// sentencias
+
+//alert('Hola'); alert('Mundo');
+
+//alert('Hola')
+//alert('Mundo')
+
+
+// Variables
+
+let mensajes,
+mensaje = "Hola Mundio"
+alert(mensaje)
+
+
+// Nombrar variables con camel case
+nombreUno= "Ivan";
+nombreDos= "David";
+
+// Nombrar como underscore
+nombre_uno = "Ivan";
+nombre_dos = "David";
+
+// Nombre no comunes
+let $ = 1;
+let _ = 2;
+
+
+// Constantes
+
+const PI = 3.14;
+const COLOR_ROJO = "#FFFFF"
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi_dos);
+
+const id_account = get_id_account(45);
